a8f489bd1d921574d1111c81bcf1cc3f371f68b4
Sameh92 <Samehsamara92@gmail.com>
2022-10-09 10:32:44 +0800

    SeedData

--- Infrastructure/Data/StoreContextDB.cs
+++ Infrastructure/Data/StoreContextDB.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Core.Entities;
+using Infrastructure.Data.Config;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
@@ -14,5 +16,13 @@ namespace Infrastructure.Data
             
         }
         public DbSet<Product> Products {get;set;}
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
